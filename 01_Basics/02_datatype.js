60415077bb52037e6b35ff9a052978de39ce32bf
Priyanshusharma1410 <152589744+Priyanshusharma1410@users.noreply.github.com>
2024-02-17 11:13:46 +0000

    datatype

---
+++ 01_Basics/02_datatype.js
@@ -0,0 +1,36 @@
+// "use strict"; //treat all JS code as newer Version.
+// aleart (3+3); //we are using nodejs, not browser.
+console.log(3
+    +
+    3) //code readability shiuld be high
+console.log("hii")
+
+let name = "priyanshu"
+let age = 20
+let isLoggedIn = false
+let state;
+
+
+//datatypes
+//number =>2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+
+
+// object
+
+console.log(typeof("hiii"));
+console.log(typeof(0.985093));
+
+console.log(typeof(undefined));//undefined
+console.log(typeof(null));//object
+
+
+
+
+

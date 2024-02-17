759648d4e2ef9f0a23c14ab60bd8db49c6c9ba84
Priyanshusharma1410 <152589744+Priyanshusharma1410@users.noreply.github.com>
2024-02-17 17:24:09 +0530

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score1 = "33abc "
+// console.log(typeof score1);
+
+let valueInNumber1 = Number(score1)
+// console.log(typeof (valueInNumber1)); //number
+
+// console.log(valueInNumber1); //NaN
+
+let score2 = "priyanshu"
+// console.log(typeof score2);
+
+let valueInNumber2 = Number(score2)
+// console.log(typeof (valueInNumber2)); //number
+
+// console.log(valueInNumber2); //NaN
+
+let score3 = false
+// console.log(typeof (score3));
+
+let valueInNumber3 = Number(score3)
+// console.log(typeof (valueInNumber3)); //number
+
+// console.log(valueInNumber3); //NaN
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(isLoggedIn);
+console.log(booleanisLoggedIn);

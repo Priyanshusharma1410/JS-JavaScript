bd274d091cf05a1ed20e6134d67f28c7426647e6
Priyanshusharma1410 <152589744+Priyanshusharma1410@users.noreply.github.com>
2024-02-18 16:35:37 +0000

    summary

---
+++ 01_Basics/datatype-summary.js
@@ -0,0 +1,23 @@
+//primitive
+// 7 types :String, Number, Boolean, null, undefined, Symbol, BigInt.
+
+const score = 100.3
+console.log(score);
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+// const bigNumber = 3456543576654356754n
+
+//Refrence (Non primitive)
+//Array, Object, Functions
+
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+const myFunction = function(){
+    console.log("Hello world");
+}

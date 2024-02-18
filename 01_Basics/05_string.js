e78372e63ed610261d37f1c85cae35ba941e6060
Priyanshusharma1410 <152589744+Priyanshusharma1410@users.noreply.github.com>
2024-02-18 16:06:54 +0000

    string

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,28 @@
+const name = "priyanshu sharma"
+const repoCount = 50
+// console.log(name);
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+//``->use this while using string.
+const gameName = new String('priyanhu sharma-PS')
+// console.log(gameName[9]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('a'));
+
+const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+const newStringOne="     Ayush    Sharma   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+
+

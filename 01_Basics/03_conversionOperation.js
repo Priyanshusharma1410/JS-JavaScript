9fcb1ae0d67d4ed82bd56a0bf859fb6a65f4eec7
Priyanshusharma1410 <152589744+Priyanshusharma1410@users.noreply.github.com>
2024-02-18 06:07:57 +0000

    operators

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -25,5 +25,47 @@ let valueInNumber3 = Number(score3)
 let isLoggedIn = 1
 
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(isLoggedIn);
-console.log(booleanisLoggedIn);
+// console.log(isLoggedIn);
+// console.log(booleanisLoggedIn);
+
+//*****************************Operations*********************************************//
+
+let value = 3
+let negValue = -value
+// console.log(value);
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+let amount=+9+1+"2"
+// console.log(typeof amount);
+// console.log("1" + 2);
+
+// console.log(1 + 2+"9");//39
+
+
+// console.log( (3 + 4) * 5 % 3 );// 35%3 = 2
+// console.log(+" ");// 0
+// console.log(+true);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+// console.log(gameCounter);
+
+
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
